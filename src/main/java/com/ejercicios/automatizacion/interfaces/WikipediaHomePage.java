782243a4fdbb6b100a70e76583faa6aad465a563
Fabian Martinez <fabi.386@hotmail.com>
2022-07-06 14:07:50 -0500

    Proyecto funcional de automatización

--- src/main/java/com/ejercicios/automatizacion/interfaces/WikipediaHomePage.java
+++ src/main/java/com/ejercicios/automatizacion/interfaces/WikipediaHomePage.java
@@ -1,9 +1,15 @@
 package com.ejercicios.automatizacion.interfaces;
 
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
 import net.thucydides.core.annotations.DefaultUrl;
 
 
 @DefaultUrl("https://www.wikipedia.org")
 public class WikipediaHomePage extends PageObject {
+     public final static Target CAMPO_TEXTO = Target.the("Caja de texto").locatedBy("/html/body/div[3]/form/fieldset/div/input");
+     public final static Target BOTON_INGRESAR = Target.the("Boton de ingresar").locatedBy("/html/body/div[3]/form/fieldset/button");
+
+     private WikipediaHomePage() {
+     }
 }
--- src/test/java/stepsdefinitions/WikipediaStepDefinitions.java
+++ src/test/java/stepsdefinitions/WikipediaStepDefinitions.java
@@ -1,6 +1,9 @@
 package stepsdefinitions;
 
+import com.ejercicios.automatizacion.interfaces.PaginaBusqueda;
 import com.ejercicios.automatizacion.interfaces.WikipediaHomePage;
+import com.ejercicios.automatizacion.questions.RespuestaBusqueda;
+import com.ejercicios.automatizacion.taks.IngresarTexto;
 import io.cucumber.java.Before;
 import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
@@ -8,8 +11,13 @@ import io.cucumber.java.es.Entonces;
 import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+import org.hamcrest.core.Is;
 
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class WikipediaStepDefinitions {
 
@@ -17,21 +25,20 @@ public class WikipediaStepDefinitions {
     public void setTheStage() {
         OnStage.setTheStage(new OnlineCast());
     }
+
     @Dado("que carolina esta en la pagina wikipedia")
     public void queCarolinaEstaEnLaPaginaWikipedia() {
         theActorCalled("Carolina").attemptsTo(Open.browserOn().the(WikipediaHomePage.class));
-
     }
 
     @Cuando("ingresa la palabra {string}")
-    public void ingresaLaPalabra(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void ingresaLaPalabra(String palabraIngresada) {
+        theActorInTheSpotlight().attemptsTo(IngresarTexto.enElCampo(palabraIngresada));
     }
 
     @Entonces("El sistema le muestra los resultados en pantalla")
     public void elSistemaLeMuestraLosResultadosEnPantalla() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        WaitUntil.the(PaginaBusqueda.CAMPO_TEXTO.getCssOrXPathSelector(), isVisible());
+        theActorInTheSpotlight().should(seeThat(RespuestaBusqueda.deLaPagina(), Is.is("Fisica")));
     }
 }
